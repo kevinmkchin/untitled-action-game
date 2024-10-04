45d093cf8f19ad84fb3dc85f9d1b52282d2c70c6
Kevin Chin <44921110+kevinmkchin@users.noreply.github.com>
2024-10-04 17:17:26 -0400

    build.bat checks all params

--- build.bat
+++ build.bat
@@ -6,10 +6,25 @@
 ::   Sets up BUILDINFO.H
 
 
+set ShowMSVCOptions=0
+set ReleaseMode=0
+
+:ParamCheck :: Loop through all parameters
+if [%1] == [] goto EndParamCheck
+if /I "%1" == "showoptions" (set ShowMSVCOptions=1)
+if /I "%1" == "-s"          (set ShowMSVCOptions=1)
+if /I "%1" == "release"     (set ReleaseMode=1)
+if /I "%1" == "-r"          (set ReleaseMode=1)
+:: Shift the parameters to the left
+shift 
+goto ParamCheck
+:EndParamCheck
+
 :: BUILDINFO.H ::
-set IsInternalBuild=1 
 set CurrentDir=%cd%
 set CurrentDir=%CurrentDir:\=/%
+:: maybe InternalBuild should be always if not Release mode?
+set IsInternalBuild=1
 echo #pragma once                                        > code\BUILDINFO.H
 echo #define PROJECT_WORKING_DIR "%CurrentDir%/wd/"     >> code\BUILDINFO.H
 echo #define MESA_WINDOWS 1                             >> code\BUILDINFO.H
@@ -19,30 +34,19 @@ echo #define INTERNAL_BUILD %IsInternalBuild%           >> code\BUILDINFO.H
 set CommonCompilerFlags=-nologo /EHsc /W3 /we4239 /wd4996 /MP /I"..\ext" /I"..\ext\gl3w" /I"..\ext\sdl\include" /I"..\ext\sdl2_mixer\include" /I"..\ext\assimp\include"
 set CommonLinkerFlags=/incremental:no /opt:ref /subsystem:console shell32.lib opengl32.lib dwmapi.lib ole32.lib /LIBPATH:"..\ext\sdl\lib\x64" SDL2.lib SDL2main.lib /LIBPATH:"..\ext\sdl2_mixer\lib\x64" SDL2_mixer.lib /LIBPATH:"..\ext\assimp\lib" assimp-vc142-mt.lib
 
-:: Debug or Release
+:: Debug or Release ::
 set OutputExecutable=GAME.exe
-if "%1"=="release" (
-    goto SetRelease
-) else if "%1"=="r" (
-    goto SetRelease
+if %ReleaseMode%==0 (
+    set OutputFolder=Debug
+    set CommonCompilerFlags=%CommonCompilerFlags% /Zi
+    echo Build %OutputExecutable% in Debug mode
 ) else (
-    goto SetDebug
+    set OutputFolder=Release
+    set CommonCompilerFlags=%CommonCompilerFlags% /O2 /DNDEBUG
+    echo Build %OutputExecutable% in Release mode
 )
 
-:SetDebug
-set OutputFolder=Debug
-set CommonCompilerFlags=%CommonCompilerFlags% /Zi
-echo Build %OutputExecutable% in Debug mode
-goto StartBuild
-
-:SetRelease
-set OutputFolder=Release
-set CommonCompilerFlags=%CommonCompilerFlags% /O2 /DNDEBUG
-echo Build %OutputExecutable% in Release mode
-goto StartBuild
-
 :: COPY DLLS ::
-:StartBuild
 if not exist build mkdir build
 if not exist build\%OutputFolder% mkdir build\%OutputFolder%
 if not exist build\%OutputFolder%\SDL2.dll copy ext\sdl\lib\x64\SDL2.dll build\%OutputFolder%\SDL2.dll > nul
@@ -50,14 +54,23 @@ if not exist build\%OutputFolder%\SDL2_mixer.dll copy ext\sdl2_mixer\lib\x64\SDL
 if not exist build\%OutputFolder%\assimp-vc142-mt.dll copy ext\assimp\lib\assimp-vc142-mt.dll build\%OutputFolder%\assimp-vc142-mt.dll >nul
 if not exist build\%OutputFolder%\renderdoc.dll copy ext\renderdoc.dll build\%OutputFolder%\renderdoc.dll >nul
 
-:: CL ::
-:: echo Compiler Flags: %CommonCompilerFlags%
-:: echo Linker Flags: %CommonLinkerFlags%
+:: cl.exe ::
+if %ShowMSVCOptions%==1 (
+    echo:
+    echo Compiler Flags:
+    echo %CommonCompilerFlags%
+    echo:
+    echo Linker Flags: 
+    echo %CommonLinkerFlags%
+    echo:
+)
 echo Build started: %time%
 if %IsInternalBuild%==0 (echo INTERNAL_BUILD 0) else (echo INTERNAL_BUILD 1) 
+echo:
 pushd build
 cl %CommonCompilerFlags% /Fe%OutputFolder%\%OutputExecutable% ..\code\GAME.CPP /link %CommonLinkerFlags%
 popd
+echo:
 if %errorlevel% neq 0 (
     exit /b %errorlevel%
 ) else (
