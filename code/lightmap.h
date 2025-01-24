a3a4525e22d0161f62ebfd9dd1e118556a873d3c
Kevin Chin <44921110+kevinmkchin@users.noreply.github.com>
2025-01-23 23:13:17 -0500

    moved lightmapping code out of editor.cpp

--- code/lightmap.h
+++ code/lightmap.h
@@ -11,3 +11,16 @@ struct lm_face_t
     i32 w = -1;
     i32 h = -1;
 };
+
+struct game_map_build_data_t
+{
+    ByteBuffer Output;
+
+    int TotalFaceCount = 0;
+    std::unordered_map<u32, std::vector<float>> VertexBuffers;
+    std::vector<vec3> ColliderWorldPoints;
+    std::vector<u32> ColliderSpans;
+};
+
+void BakeStaticLighting(game_map_build_data_t& BuildData);
+
